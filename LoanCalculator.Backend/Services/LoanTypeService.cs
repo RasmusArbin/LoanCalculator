d385570cfc944ca82a732a95fe0b4662b5304d75
Rasmus Arbin <rasmus.jonasson91@gmail.com>
2016-12-14 15:48:52 +0100

    Added static resource as data source

--- LoanCalculator.Backend/Services/LoanTypeService.cs
+++ LoanCalculator.Backend/Services/LoanTypeService.cs
@@ -6,20 +6,26 @@ namespace LoanCalculator.Backend.Services
 {
     public class LaonTypeService: LoanCalculatorService
     {
+        private static LoanTypeBO _loanType = new LoanTypeBO()
+        {
+            LoanTypeId = 1,
+            Guid = Guid.NewGuid(),
+            Name = "Huslån",
+            Percent = 3.5,
+            Created = DateTime.Now,
+            Modified = DateTime.Now
+        };
         public List<LoanTypeBO> GetAllLoanTypes()
         {
             return new List<LoanTypeBO>()
             {
-                new LoanTypeBO()
-                {
-                    LoanTypeId = 1,
-                    Guid = Guid.NewGuid(),
-                    Name = "Huslån",
-                    Percent = 3.5,
-                    Created = DateTime.Now,
-                    Modified = DateTime.Now
-                }
+                _loanType
             };
+        }
+
+        public LoanTypeBO GetById()
+        {
+            return _loanType;
         }
     }
 }
