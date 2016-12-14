bd013664a01e2a2e23aacc522ee15d647c7f2d41
Rasmus Arbin <rasmus.jonasson91@gmail.com>
2016-12-14 17:42:26 +0100

    Added CalculatePaymentPlan

--- LoanCalculator.Api/Controllers/LoanTypeController.cs
+++ LoanCalculator.Api/Controllers/LoanTypeController.cs
@@ -1,22 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using LoanCalculator.Api.Models;
+using LoanCalculator.Backend.BO;
 using LoanCalculator.Backend.Services;
 
 namespace LoanCalculator.Api.Controllers
 {
+    [RoutePrefix("Api/LoanTypes")]
     public class LoanTypeController: LoanCalculatorApiController
     {
+        [Route("")]
         [HttpGet]
         public IEnumerable<LoanTypeModel> GetAllLoanTypes()
         {
-            return ServiceProvider.GetService<LaonTypeService>().GetAllLoanTypes().Select(l => new LoanTypeModel()
+            return ServiceProvider.GetService<LoanTypeService>().GetAllLoanTypes().Select(l => new LoanTypeModel()
             {
                 Id = l.Guid,
                 Name = l.Name,
                 Percent = l.Percent
             });
+        }
+
+        [Route("CalculatePaymentPlan")]
+        [HttpGet]
+        public double CalculatePaymentPlan(Guid loanTypeId, double loanAmount, int yearCount)
+        {
+            return ServiceProvider.GetService<LoanTypeService>()
+                .CalculatePaymentPlan(loanTypeId, loanAmount, yearCount);
         }
     }
 }
--- LoanCalculator.Backend/Services/LoanTypeService.cs
+++ LoanCalculator.Backend/Services/LoanTypeService.cs
@@ -4,7 +4,7 @@ using LoanCalculator.Backend.BO;
 
 namespace LoanCalculator.Backend.Services
 {
-    public class LaonTypeService: LoanCalculatorService
+    public class LoanTypeService: LoanCalculatorService
     {
         private static LoanTypeBO _loanType = new LoanTypeBO()
         {
@@ -23,9 +23,50 @@ namespace LoanCalculator.Backend.Services
             };
         }
 
-        public LoanTypeBO GetById()
+        public LoanTypeBO GetByGuid(Guid loanTypeGuid)
         {
             return _loanType;
+        }
+
+        /// <summary>
+        /// Gets a payment plan that tells the user how much he or she must pay back to be dept free after desired year count
+        /// </summary>
+        /// <param name="loanAmount">Total amoun loaned from bank</param>
+        /// <param name="yearCount">The desired year count to pay back the loan</param>
+        /// <returns>How much money the user must pay every month to to be out of dept after desired year count</returns>
+        public double CalculatePaymentPlan(Guid loanTypeGuid, double loanAmount, int yearCount)
+        {
+            LoanTypeBO loanType = GetByGuid(loanTypeGuid);
+
+            int monthCount = GetMonth(yearCount);
+
+            return CalculateTotalAmmountToPayBack(loanAmount, monthCount, loanType.Percent) / monthCount;
+        }
+
+        /// <summary>
+        /// Get total amount to pay back to the bank
+        /// </summary>
+        /// <param name="loanAmount">Total amoun loaned from bank</param>
+        /// <param name="monthCount">The desired month count to pay back the loan</param>
+        /// <param name="percent">The interest on the loan</param>
+        /// <returns>Total amount of dept after a certan year count</returns>
+        private double CalculateTotalAmmountToPayBack(double loanAmount, int monthCount, double percent)
+        {
+            double copiedLoanAmount = loanAmount;
+            double totalInterestAmount = 0;
+            double amortization = copiedLoanAmount/monthCount;
+            for (int i = 0; i < monthCount; i++)
+            {
+                totalInterestAmount += copiedLoanAmount*percent/100/12;
+                copiedLoanAmount -= amortization;
+            }
+
+            return loanAmount + totalInterestAmount;
+        }
+
+        private int GetMonth(int years)
+        {
+            return years*12;
         }
     }
 }
